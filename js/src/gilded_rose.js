2f6ab4f464fe66842fb6b834bea7e06632964265
Alex Fernández <alex.fernandez@logtrust.com>
2018-12-16 10:47:27 +0100

    Refactor: Single comparison block.

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -16,25 +16,22 @@ function update_item(item) {
   if (item.name == 'Sulfuras, Hand of Ragnaros') {
     return;
   }
+  item.sell_in = item.sell_in - 1;
   if (item.name == 'Aged Brie') {
     item.quality = item.quality + 1
   } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-      if (item.sell_in < 6) {
-        item.quality = item.quality + 3
-      } else if (item.sell_in < 11) {
-        item.quality = item.quality + 2
-      } else {
-        item.quality += 1
-      }
+    if (item.sell_in < 0) {
+      item.quality = 0
+    } else if (item.sell_in < 5) {
+      item.quality = item.quality + 3
+    } else if (item.sell_in < 10) {
+      item.quality = item.quality + 2
+    } else {
+      item.quality += 1
+    }
   } else {
-    item.quality = item.quality - 1
-  }
-  item.sell_in = item.sell_in - 1;
-  if (item.sell_in < 0) {
-    if (item.name == 'Aged Brie') {
-      item.quality = item.quality + 1
-    } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-      item.quality = item.quality - item.quality
+    if (item.sell_in < 0) {
+      item.quality = item.quality - 2
     } else {
       item.quality = item.quality - 1
     }
