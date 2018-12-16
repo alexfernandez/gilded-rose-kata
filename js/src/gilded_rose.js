62ed790d9a2a3013c370b5d46368dc145fd66fc8
Alex Fernández <alex.fernandez@logtrust.com>
2018-12-16 10:32:39 +0100

    Refactor: single comparison for Sulfuras.

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -13,11 +13,12 @@ function update_quality() {
 }
 
 function update_item(item) {
+  if (item.name == 'Sulfuras, Hand of Ragnaros') {
+    return;
+  }
   if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
     if (item.quality > 0) {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.quality = item.quality - 1
-      }
+      item.quality = item.quality - 1
     }
   } else {
     item.quality = item.quality + 1
@@ -30,16 +31,12 @@ function update_item(item) {
       }
     }
   }
-  if (item.name != 'Sulfuras, Hand of Ragnaros') {
-    item.sell_in = item.sell_in - 1;
-  }
+  item.sell_in = item.sell_in - 1;
   if (item.sell_in < 0) {
     if (item.name != 'Aged Brie') {
       if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
+          item.quality = item.quality - 1
         }
       } else {
         item.quality = item.quality - item.quality
